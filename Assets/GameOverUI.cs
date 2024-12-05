a4d04a609af60f73703e735b03b8e5978449eefc
rap <132430307+RaphChristophr@users.noreply.github.com>
2024-12-06 04:40:56 +0800

    ganti ui game over
    ok

--- Assets/GameOverUI.cs
+++ Assets/GameOverUI.cs
@@ -3,9 +3,22 @@ using UnityEngine.SceneManagement;
 
 public class GameOverUI : MonoBehaviour
 {
-    public void Restart() 
+    public void Restart()
     {
+        // Mengambil nama scene yang sedang aktif
+        string currentSceneName = SceneManager.GetActiveScene().name;
+
+        // Mengatur waktu agar berjalan normal
         Time.timeScale = 1;
-        SceneManager.LoadScene("Hutan");
+
+        // Memuat ulang scene yang sedang aktif
+        SceneManager.LoadScene(currentSceneName);
+    }
+
+    public void GoToMainMenu()
+    {
+        // Memuat scene MainMenu
+        SceneManager.LoadScene("MainMenu");
     }
 }
+    
